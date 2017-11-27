ea21fbcc5da75446e085d0a35fe1bf821046f9ba
ereboz <ereboz8@gmail.com>
2017-11-27 14:31:00 -0800

    Most kinks worked out and is responsive

---
+++ scripts/sentry-safe.js
@@ -0,0 +1,12 @@
+
+$("li.exterior").click(function(){
+    $("img.sentry-safe-views").attr("src","images/sentry-safe/safe-exterior.png");
+});
+
+$("li.interior").click(function(){
+    $("img.sentry-safe-views").attr("src","images/sentry-safe/safe-interior.png");
+});
+
+$("li.capacity").click(function(){
+    $("img.sentry-safe-views").attr("src","images/sentry-safe/safe-capacity.png");
+});
